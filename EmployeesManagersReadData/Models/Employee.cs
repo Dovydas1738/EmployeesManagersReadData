5656a2f23a4a8b76eeaa1fd68799206f1ce6ce07
Dovydas Montvydas <dovism@gmail.com>
2024-07-29 09:34:29 +0300

    Add project files.

---
+++ EmployeesManagersReadData/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using EmployeesManagersReadData.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesManagersReadData.Models
+{
+    internal class Employee : IEmployee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void ReadData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EmployeesManagersReadData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyProgram
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
